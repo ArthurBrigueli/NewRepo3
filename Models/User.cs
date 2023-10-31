608ab8b93537c22ee9145fab0a7cfef3730b8720
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-10-31 15:00:47 -0300

    adição de model user

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace pimfo.Models
+{
+    public class User
+    {
+        [Key]
+        [DisplayName("ID")]
+        public int id { get; set; }
+
+        [DisplayName("Usuario")]
+        public string usuario { get; set; }
+
+        [DisplayName("Senha")]
+        public string senha { get; set; }
+    }
+}
